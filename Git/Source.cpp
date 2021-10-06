668587293d11d62c9dd4fd0e7c2b8c5d4c28195e
ForSchool13372 <gobbler828@gmail.com>
2021-10-06 10:30:52 -0400

    Added Functions

--- Git/Source.cpp
+++ Git/Source.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 using namespace std;
 
-
+//Set up SDL and all subsystems.
 void Init() {
 	cout << "Initializing game..." << endl;
 }
+//Handle events function.
+void HandleEvents() {
+	cout << "Processing..." << endl;
+}
+//Update function. Process game objects.
+void Update() {
+	cout << "Initializing game..." << endl;
+}
+//Render function. Updates the window.
+void Render() {
+	cout << "Rendering..." << endl;
+}
+
+void Clean() {
+	cout << "Cleaning..." << endl;
+}
+
 //Run Function. Contains the main game loop.
 int Run() {
 	
@@ -13,8 +30,16 @@ int Run() {
 
 	Init();
 	while (isRunning) {//This is our game loop.
-
+		HandleEvents();	//Input.
+		Update();	//Processing
+		Render();	//Output.
+		cout << "\nDo you want to quit? [y/n]: ";
+		cin >> input;
+		if (input == 'y') {
+			isRunning = false;
+		}
 	}
+	Clean();
 	return 0;
 }
 
