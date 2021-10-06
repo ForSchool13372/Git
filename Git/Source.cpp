81ee806c968a9ebd2212f2bd043949f84eb6ac13
ForSchool13372 <gobbler828@gmail.com>
2021-10-06 09:51:46 -0400

    Added main, Run and init functions.

--- Git/Source.cpp
+++ Git/Source.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
 
+void Init() {
+	cout << "Initializing game..." << endl;
+}
+//Run Function. Contains the main game loop.
+int Run() {
+	
+	bool isRunning = true;
+	char input;
+
+	Init();
+	while (isRunning) {//This is our game loop.
+
+	}
 	return 0;
+}
+
+
+// main function. Entry point of every C++ program.
+int main() {
+	
+	return Run();
 }
